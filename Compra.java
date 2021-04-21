131ee91025193e399305b077ace124e3918b5049
leonelcruzp <82000760+leonelcruzp@users.noreply.github.com>
2021-04-21 17:16:47 -0500

    Add files via upload

--- Compra.java
+++ Compra.java
@@ -1,76 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Persistencia;
-import java.util.List;
 
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Leonel
+ */
 public class Compra {
-    private Cliente NombreCliente;
-    private Vendedor NombreVendedor;
-    private List<Producto> ListaProductos;
+    private Cliente nombreCliente;
+    private Vendedor nombreVendedor;
+    private ArrayList<Producto> listaProductos;
     private float montoTotal;
     private float noFolio;
-    private Repartidor NombreRepartidor;  
+    private Repartidor nombreRepartidor;
     
-    public Compra(Cliente NombreCliente, Vendedor NombreVendedor, List<Producto> ListaProductos, float montoTotal, float noFolio, Repartidor NombreRepartidor){
-    this.NombreCliente = NombreCliente;
-    this.NombreVendedor = NombreVendedor;
-    this.ListaProductos = ListaProductos;
-    this.montoTotal = montoTotal;
-    this.noFolio = noFolio;
-    this.NombreRepartidor = NombreRepartidor;
+    public Compra(){
+        
+    }
+
+    public Cliente getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(Cliente nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public Vendedor getNombreVendedor() {
+        return nombreVendedor;
+    }
+
+    public void setNombreVendedor(Vendedor nombreVendedor) {
+        this.nombreVendedor = nombreVendedor;
+    }
+
+    public ArrayList<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(ArrayList<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public float getMontoTotal() {
+        return montoTotal;
+    }
+
+    public void setMontoTotal(float montoTotal) {
+        this.montoTotal = montoTotal;
+    }
+
+    public float getNoFolio() {
+        return noFolio;
+    }
+
+    public void setNoFolio(float noFolio) {
+        this.noFolio = noFolio;
+    }
+
+    public Repartidor getNombreRepartidor() {
+        return nombreRepartidor;
+    }
+
+    public void setNombreRepartidor(Repartidor nombreRepartidor) {
+        this.nombreRepartidor = nombreRepartidor;
+    }
+    
 }
-
-public Cliente getNombreCliente(){
-    return NombreCliente;
-}
-
-public void setNombreCliente(Cliente NombreCliente){
-    this.NombreCliente = NombreCliente;
-}
-
-public Vendedor getNombreVendedor(){
-    return NombreVendedor;
-}
-
-public void setNombreVendedor(Vendedor NombreVendedor){
-    this.NombreVendedor = NombreVendedor;
-}
-      
-public List<Producto> getListaProductos(){
-    return ListaProductos;
-}
-
-public void setListaProductos(List<Producto> ListaProductos){
-    this.ListaProductos = ListaProductos;
-}
-
-public float getmontoTotal(){
-    return montoTotal;
-}
-
-public void setmontoTotal(float montoTotal){
-    this.montoTotal = montoTotal;
-}
-
-public float getnoFolio(){
-    return noFolio;
-}
-
-public void setnoFolio(float noFolio){
-    this.noFolio = noFolio;
-}
-
-public Repartidor getNombreRepartidor(){
-    return NombreRepartidor;
-}
-
-public void setNombreRepartidor(Repartidor NombreRepartidor){
-    this.NombreRepartidor = NombreRepartidor;
-}
-}
-
-
-
-
-
-
-
-
